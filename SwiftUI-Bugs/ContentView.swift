9b0d98dce202443142dd2a92cf224e429cce82f3
Ilya Cherkasov <cherkasov-i@surf.dev>
2022-07-15 12:27:47 +0300

    Initial Commit

---
+++ SwiftUI-Bugs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Bugs
+//
+//  Created by Ilya Cherkasov on 15.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Bugs/SwiftUI_BugsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BugsApp.swift
+//  SwiftUI-Bugs
+//
+//  Created by Ilya Cherkasov on 15.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BugsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
